a57f9e5dc1d3966d273b9c48522659774c0d1980
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-18 16:18:51 +0530

    fix: ignore logs

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import * as React from "react";
 import AppContextProvider from "./AppContext";
 import AppWrapper from "./AppWrapper";
+import { LogBox } from "react-native";
 
 export default function App() {
+    LogBox.ignoreAllLogs();
+
     return (
         <AppContextProvider>
             <AppWrapper />
